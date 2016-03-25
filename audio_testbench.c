add081053fe2068f023d3fb7a09221830af91108
Bryce Cater <brycedcarter@gmail.com>
2016-03-25 12:15:31 -0400

    completed audio testbench added sound samples for testbench

--- audio_testbench.c
+++ audio_testbench.c
@@ -8,20 +8,35 @@
 
 #include "FIFO.h"
 #include "FIFO_low_handler.h"
+#include "sample_data/snd_sample.c"
 #include "sample_pool.h"
 #include "EmbeddedAudioTypes.h"
 #include "zedboard_freertos.h"
 
+unsigned int* current_sample;
+
+void testbench_pool_low_callback(sample_pool_t* pPool, int number_needed)
+{
+    sample_pool_fill_samples(pPool, (sample_t*)current_sample, number_needed);
+    current_sample += number_needed* sizeof(sample_t*);
+}
+
 int main()
 {
+    current_sample = snd_samples;
+
     FIFO_t FIFO = {FIFO_BASE_ADDR};
     FIFO_init(&FIFO);
 
     sample_pool_t main_sample_pool;
     main_sample_pool.size = POOL_SIZE;
+    main_sample_pool.request_samples_callback = testbench_pool_low_callback;
     sample_pool_allocate(&main_sample_pool);
     main_sample_pool.thresh = POOL_THRESH;
 
     setup_FIFO_low_handler(&main_sample_pool, &FIFO);
 }
+
+typedef int myfunnyvar;
+
 
